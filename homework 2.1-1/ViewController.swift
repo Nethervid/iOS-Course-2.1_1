f3e0ea16ab0da06acfda4c3ce0c83ac084108f9e
Artem <artem2097@gmail.com>
2020-08-19 14:15:35 +0300

    Commit of first exercise

---
+++ homework 2.1-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework 2.1-1
+//
+//  Created by Артем on 19.08.2020.
+//  Copyright © 2020 Artem Golubev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
